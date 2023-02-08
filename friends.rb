17f68c2b0f8ab732f13a6b802c3ca1a5cea3edec
Matthew Leahy <lordgibtonic@gmail.com>
2023-02-07 17:04:15 -0700

    Add elsif

--- friends.rb
+++ friends.rb
@@ -1,6 +1,8 @@
-best_friend = "Daniel" 
+best_friend = "Matthew" 
 if best_friend == "Daniel"
     puts "Oh it's you :("
+elsif best_friend == "Luana"
+    puts "Hey smoking"
 else 
     puts "Welcome new friend!!!"
 end
